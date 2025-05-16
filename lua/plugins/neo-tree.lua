c0feaf9748f6f92a27739936512d6ada1ba54b29
Webber Wu <webberwu.tw@gmail.com>
2025-05-16 17:38:55 +0800

    neo-tree.nvim: add 'oa' key mapping to add files to Avante sidebar

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,7 +13,8 @@ return {
             require("neo-tree").setup({
                 window = {
                     mappings = {
-                        ["<C-t>"] = "open_tabnew"
+                        ["<C-t>"] = "open_tabnew",
+                        ["oa"] = "avante_add_files",
                     }
                 },
                 filesystem = {
@@ -22,6 +23,30 @@ return {
                         hide_dotfiles = false,
                         hide_gitignored = false,
                     }
+                },
+                commands = {
+                    -- from https://github.com/yetone/avante.nvim/blob/main/README.md#neotree-shortcut
+                    avante_add_files = function(state)
+                        local node = state.tree:get_node()
+                        local filepath = node:get_id()
+                        local relative_path = require('avante.utils').relative_path(filepath)
+
+                        local sidebar = require('avante').get()
+
+                        local open = sidebar:is_open()
+                        -- 確保 avante 側邊欄已打開
+                        if not open then
+                            require('avante.api').ask()
+                            sidebar = require('avante').get()
+                        end
+
+                        sidebar.file_selector:add_selected_file(relative_path)
+
+                        -- 刪除 neo tree 緩沖區
+                        if not open then
+                            sidebar.file_selector:remove_selected_file('neo-tree filesystem [1]')
+                        end
+                    end,
                 },
             })
         end
