f7382f34a3012f7b04a564828e1529790912c71d
Webber Wu <webberwu.tw@gmail.com>
2025-06-10 18:12:47 +0800

    feat: move help buffer to new tab automatically

--- init.lua
+++ init.lua
@@ -80,3 +80,13 @@ vim.api.nvim_create_autocmd("CursorHold", {
     pattern = "*",
     command = "checktime",
 })
+
+-- Move help buffer to new tab automatically
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*.txt",
+    callback = function()
+        if vim.bo.buftype == "help" then
+            vim.cmd("wincmd T")
+        end
+    end,
+})
