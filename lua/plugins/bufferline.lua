5f62f9beb0eca13a2d1739c246134ebaec4e0b9b
Webber Wu <webberwu.tw@gmail.com>
2025-07-11 19:25:13 +0800

    feat(bufferline): disable buffer name truncation

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,11 +5,13 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
+        -- :h bufferline-configuration
         require("bufferline").setup({
             options = {
                 mode = "tabs",
                 numbers = "ordinal",
                 show_close_icon = false,
+                truncate_names = false,
             },
             highlights = {
                 buffer_selected = {
